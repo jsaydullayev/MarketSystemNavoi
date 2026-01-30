cba5d08b384907b93f43a71aeae2c650c0a2d6dd
Joo <saydullayev_jahongir@mail.ru>
2026-01-30 18:06:22 +0500

    new code

--- MarketSystem.Domain/Entities/Branch.cs
+++ MarketSystem.Domain/Entities/Branch.cs
@@ -9,7 +9,6 @@ public class Branch : BaseEntity
     public string? Phone { get; set; }
 
     // Navigation properties
-    public ICollection<User> Users { get; set; } = new List<User>();
     public ICollection<BranchProduct> BranchProducts { get; set; } = new List<BranchProduct>();
     public ICollection<Sale> Sales { get; set; } = new List<Sale>();
     public ICollection<Zakup> Zakups { get; set; } = new List<Zakup>();
