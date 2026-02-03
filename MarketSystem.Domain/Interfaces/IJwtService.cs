f773c02c124c9daa27761ab78d9c7ebc9e8ce2ea
Joo <saydullayev_jahongir@mail.ru>
2026-02-03 20:22:50 +0500

    new code

--- MarketSystem.Domain/Interfaces/IJwtService.cs
+++
@@ -1,7 +0,0 @@
-namespace MarketSystem.Domain.Interfaces;
-
-public interface IJwtService
-{
-    TokenDto GenerateToken(Entities.User user, bool populateExp);
-    Tuple<bool, string?> ValidateAndGetUser(string token);
-}
