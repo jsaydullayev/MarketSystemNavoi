d2a524ddefd1154f3723545ac7bfecf2db950b50
Joo <saydullayev_jahongir@mail.ru>
2026-02-23 10:29:04 +0500

    new code

--- MarketSystem.Client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ MarketSystem.Client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import connectivity_plus
 import file_selector_macos
-import open_file_mac
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
