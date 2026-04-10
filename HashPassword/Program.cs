51b50c7c37a4189ff16a612e092bf2fe0ab48ff3
joo <joo@local.dev>
2026-04-10 17:50:33 +0500

    new code

---
+++ HashPassword/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string password = args.Length > 0 ? args[0] : "jaxongir2005";
+        string hash = BCrypt.Net.BCrypt.HashPassword(password);
+        Console.WriteLine(hash);
+    }
+}
