93007856e48b33762f8ed7c7d9d8d6bc084344fd
joo <joo@local.dev>
2026-04-24 10:12:42 +0500

    new code

--- MarketSystem.Domain/Entities/CashWithdrawal.cs
+++ MarketSystem.Domain/Entities/CashWithdrawal.cs
@@ -4,7 +4,7 @@ namespace MarketSystem.Domain.Entities;
 
 public class CashWithdrawal : BaseEntity
 {
-    public Guid Id { get; set; }
+    public new Guid Id { get; set; }
     public decimal Amount { get; set; }
     public string Comment { get; set; } = string.Empty;
     public DateTime WithdrawalDate { get; set; }
