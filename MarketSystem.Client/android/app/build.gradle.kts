5215cb7750f29798ef29fe451330de02e737f477
joo <joo@local.dev>
2026-04-27 11:05:41 +0500

    new code

--- MarketSystem.Client/android/app/build.gradle.kts
+++ MarketSystem.Client/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "uz.strotech"
+        applicationId = "strotech.uz"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
